ab4ae0536dd84542778eeca536470225778bcdee
haiderGithubOfficial <haider.official.gfx@gmail.com>
2022-09-16 00:02:06 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-container">
+      <div className="category-container">
+        <div className="category-body-container">
+          {/* <img /> */}
+          <h2>hats</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          {/* <img /> */}
+          <h2>jackets</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          {/* <img /> */}
+          <h2>sneakers</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          {/* <img /> */}
+          <h2>womens</h2>
+          <p>shop now</p>
+        </div>
+      </div>
+      <div className="category-container">
+        <div className="category-body-container">
+          {/* <img /> */}
+          <h2>mens</h2>
+          <p>shop now</p>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
