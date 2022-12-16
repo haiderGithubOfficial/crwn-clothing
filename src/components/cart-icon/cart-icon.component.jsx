ba1e7f538b1b71715a85f5d6e3e70827a8827b8a
haiderGithubOfficial <haider.official.gfx@gmail.com>
2022-12-16 14:55:43 +0500

    Revert "addtocart my implementation"
    This reverts commit 4e7848a60f5479da6ec13a97997de22a1a44ce27.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,11 +2,11 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ dropdownHandler, cartItem }) => {
+const CartIcon = ({ dropdownHandler }) => {
   return (
     <div onClick={dropdownHandler} className="cart-icon-container">
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">{cartItem.length}</span>
+      <span className="item-count">0</span>
     </div>
   );
 };
